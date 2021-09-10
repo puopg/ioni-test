9e26503258330cf2bca2e0a615688357dce1f57d
Michael Chen <mchen@nianticlabs.com>
2021-09-10 09:53:49 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { IonApp, IonContent, IonButton, IonModal } from "@ionic/react";
 
-function App() {
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
+
+const ComponentA = (props: any): React.ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IonButton onClick={props.close}>Close A</IonButton>
     </div>
   );
-}
+};
+
+const ComponentB = (props: any): any => {
+  useEffect(() => {
+    alert("I SHOULD NEVER BE SEEN");
+  }, []);
+
+  return "YOU SHOULD NEVER SEE ME";
+};
+
+const App: React.FC = () => {
+  const [isOpenA, setA] = useState(false);
+
+  return (
+    <IonApp>
+      <IonContent>
+        <IonModal isOpen={isOpenA}>
+          <ComponentA close={() => setA(false)} />
+        </IonModal>
+        <IonModal isOpen={false}>
+          <ComponentB />
+        </IonModal>
+        <div>
+          Open A, Close A, and watch ComponentB Mount (it will alert on mount
+        </div>
+        <IonButton
+          onClick={() => {
+            setA(!isOpenA);
+          }}
+        >
+          Open A
+        </IonButton>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
